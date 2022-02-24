46dbaca7c24a66c4f0ba6e4569812f96c58347ab
Veronica-Alfr <veagalves@gmail.com>
2022-02-24 15:13:19 -0300

    Requisito 4 em progresso

--- src/App.js
+++ src/App.js
@@ -3,10 +3,45 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
+      cardImage: '',
+      cardRare: '',
+      cardTrunfo: false,
+      hasTrunfo: false,
+      isSaveButtonDisabled: false,
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  onSaveButtonClick = () => {
+    console.log('clicou!');
+    this.setState((prevState) => ({
+      isSaveButtonDisabled: !prevState.isSaveButtonDisabled,
+    }));
+  }
+
   render() {
     return (
       <main>
-        <Form />
+        <Form
+          { ...this.state }
+          onInputChange={ this.onInputChange }
+          onSaveButtonClick={ this.onSaveButtonClick }
+        />
         <Card />
       </main>
     );

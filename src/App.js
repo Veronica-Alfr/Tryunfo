8be9d4424c31923d6af9aebf36f8ab3b8a98982e
Veronica-Alfr <veagalves@gmail.com>
2022-02-23 22:06:50 -0300

    Requisito 3 em andamento

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   render() {
     return (
-      <Form />
+      <Card />
     );
   }
 }
